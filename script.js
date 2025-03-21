c94e19a81b728c940f2bf4865c23c2b8b12ff066
Amna Zaher <119043728+AmnaZaher@users.noreply.github.com>
2025-03-21 05:07:37 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
+//login java
 document.querySelector('form').addEventListener('submit', function(e) {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    if (!username || !password) {
-        alert("Please fill in both fields.");
-        e.preventDefault();
-    }
+    e.preventDefault(); // منع الإرسال الافتراضي للنموذج
+    
+    // إعادة توجيه المستخدم إلى الصفحة المطلوبة
+    window.location.href = "search.html"; // استبدلي "your-page.html" بالصفحة المطلوبة
 });
+
+//index java
 
 function navigateToLogin(userType) {
     window.location.href = "login.html?type=" + userType;
